7edf8a5d849dca70707b11a8de22e4732055ba01
Samar Panda <samar.panda84@gmail.com>
2015-03-31 18:16:45 +0530

    travis test update

--- spec/httpServerSpec.js
+++ spec/httpServerSpec.js
@@ -2,26 +2,24 @@ require("../app/http_server.js");
 var request = require("request");
 var base_url = "http://localhost:3000/";
 
-describe("HTTP Pixel log server is configured", function(){
-	describe("GET /", function(){
-		it("Returns status code 200", function(done){
-			request.get(base_url, function(err, res, body){
-				expect(err).toBeNull();
-				expect(res.statusCode).toBe(200);
-				done();
-			});
-		});
-		it("Returns data", function(done){
-			request.get(base_url, function(err, res, body){
-				expect(err).toBeNull();
-				expect(body).not.toBeNull();
-				done();
-			});
+describe("HTTP Pixel log server : GET calls", function(){
+
+	it("Returns status code 200", function(done){
+		request.get(base_url, function(err, res, body){
+			expect(err).toBeNull();
+			expect(res.statusCode).toBe(200);
+			done();
 		});
 	});
-});
 
-describe("Execute command and return in http request", function(){
+	it("Returns data", function(done){
+		request.get(base_url, function(err, res, body){
+			expect(err).toBeNull();
+			expect(body).not.toBeNull();
+			done();
+		});
+	});
+
 	it("Get result shell_script result via http", function(done){
 		request.get(base_url+"test-exec", function(err, res, body){
 			expect(err).toBeNull();
@@ -31,4 +29,3 @@ describe("Execute command and return in http request", function(){
 		});
 	});
 });
-
